86c6b34af6201c0da10298056ec87b80980d7ff8
ranjanisiva <ranjanisiva2798@gmail.com>
2021-06-03 17:29:22 +0530

    Add files via upload

--- demo.js
+++ demo.js
@@ -1,16 +1,1 @@
-
-const puppeteer = require('puppeteer');
-
-import reporter from './reporter';
-(async () => {
-    reporter
-      .feature('Feature')
-      .story('Story');
-      const browser = await puppeteer.launch({ headless: false,defaultViewport: null,
-        args: ['--start-maximized'] , executablePath:"C:/Users/sriranjanis/AppData/Local/Google/Chrome/Application/chrome.exe"})
-    const page = await browser.newPage()
-    await page.goto('https://www.google.com')
-    await page.waitFor(500) 
-    const screenshot = await page.screenshot({ path: `LoginPage.png` });
-    reporter.addAttachment('Screenshot', screenshot, 'LoginPage.png');
-})();
+console.log("puppeteeeeeeeeeer")
